f7362a4adb833bf954fdf0b5f61a0956ead80d43
yjfu95103 <45889635+yjfu95103@users.noreply.github.com>
2019-07-04 13:39:47 +0800

    ip2019s

---
+++ portfolio/labs/lab99/lab.js
@@ -0,0 +1,18 @@
+/*
+
+*/
+
+
+function start(e) {
+
+}
+
+/*
+https://www.w3schools.com/jsref/met_document_addeventlistener.asp
+
+document.addEventListener(event, function, useCapture)
+
+true - The event handler is executed in the capturing phase
+false- Default. The event handler is executed in the bubbling phase
+*/
+window.addEventListener( "load", start, false );
